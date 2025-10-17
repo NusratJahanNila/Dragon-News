61d0424e0699a35476330b377f0ced3c22c7d6c8
NusratJahanNila <nnusratjahan330@gmail.com>
2025-10-18 00:49:03 +0600

    added navbar

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../Components/Header';
 import LatestNews from '../Components/LatestNews';
+import Navbar from '../Components/Navbar';
 
 const HomeLayout = () => {
     return (
@@ -13,6 +14,9 @@ const HomeLayout = () => {
                 <section className='w-3/4 mx-auto my-3'>
                     <LatestNews></LatestNews>
                 </section>
+                <nav className='w-3/4 mx-auto my-3'>
+                    <Navbar></Navbar>
+                </nav>
                 <section className='left-nav'></section>
                 <section className='main-nav'>
                     <Outlet></Outlet>
